661e9f08c8d6b3f9752891c95faeb916b152c5bf
Kenan Berbic <kberbic@Kenans-MacBook-Pro.local>
2021-01-11 15:31:02 +0100

    Remove sh skript, move everthing on node

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,151 @@
-#!/usr/bin/env node
+import fs from 'fs';
+import { Command } from 'commander';
+import { exec } from 'child_process';
 
-const path = require('path');
-const { exec } = require('child_process');
-const args = process.argv.splice(2);
+import templates, {
+    capitalize,
+    execAsync
+} from './templates.js';
 
-const PATH = path.resolve(__dirname, '../');
-const command = `chmod +x ${PATH}/ms.sh; sh ${PATH}/ms.sh ${args.join(" ")} -x ${PATH}`;
+const PATH = import.meta.url.replace("/bin/index.js", "").replace("file://", "");
+const data = fs.readFileSync(`${PATH}/package.json`, {encoding: 'utf8'});
+const dataOfPackage = JSON.parse(data);
 
-exec(command, (err)=> err && console.log(err))
-    .stdout
+const program = new Command();
+program.version(dataOfPackage.version);
+
+program
+    .option('-s, --service  <name>', 'Service name without \'Service\' keyword')
+    .option('-p, --port <number>', 'Service port number - REQUIRED')
+    .option('-d, --database  <type>', 'Add database configuration, support: mongodb, postgres')
+    .option('-a, --auth  <type>', 'Add init auth on service, support: jwt, auth0, okta');
+
+program.parse(process.argv);
+
+if(!program.service){
+    console.log("Missing service name");
+    process.exit(0);
+}
+
+if(!program.port){
+    console.log("Missing service port");
+    process.exit(0);
+}
+
+console.log(`Generate configuration for ${capitalize(program.service)}Service`);
+
+const {
+    PROTO_TEMPLATE,
+    SERVICE_TEMPLATE,
+    TEST_TEMPLATE,
+    MONGODB_MODEL_TEMPLATE,
+    POSTGRES_MODEL_TEMPLATE,
+    ENVIRONMENT_TEMPLATE,
+    DOCKER_TEMPLATE
+} = templates(program);
+
+let MIDDLEWARES = 'correlation,';
+let ENV_TEMPLATE = ENVIRONMENT_TEMPLATE;
+
+await execAsync(`mkdir -p ${program.service}`);
+await execAsync(`mkdir -p ${program.service}/interfaces`);
+await execAsync(`cp -a ${PATH}/.service/. ${program.service}/.`);
+await execAsync(`rm -rf ${program.service}/node_modules`);
+await execAsync(`rm -rf ${program.service}/__tests__/*`);
+await execAsync(`mkdir -p ${program.service}/__tests__/services`);
+
+fs.writeFileSync(
+    `${program.service}/interfaces/${program.service}.proto`,
+    PROTO_TEMPLATE,
+    {encoding : 'utf8'});
+
+fs.writeFileSync(
+    `${program.service}/services/${program.service}.service.js`,
+    SERVICE_TEMPLATE,
+    {encoding : 'utf8'});
+
+fs.writeFileSync(
+    `${program.service}/__tests__/services/${program.service}.service.spec.js`,
+    TEST_TEMPLATE,
+    {encoding : 'utf8'});
+
+if(program.auth){
+    await execAsync(`cp -a ${PATH}/.providers/${program.auth}/${program.auth}.js ${program.service}/modules/.`);
+    if(program.auth === 'auth0'){
+        MIDDLEWARES += ' auth0([]),'
+        ENV_TEMPLATE += `
+AUTH0_DOMAIN=
+AUTH0_AUDIENCE=
+    `;
+    }
+    else if(program.auth === 'jwt'){
+        MIDDLEWARES += ' jwt([]),'
+        ENV_TEMPLATE += `
+JWT_SECRET=00000000
+    `;
+    }else {
+        console.log("Wrong auth parameter, only support: jwt and auth0");
+        process.exit(0);
+    }
+}
+
+if(program.database) {
+    await execAsync(`cp -a ${PATH}/.database/${program.database}/index.js ${program.service}/models/.`);
+    let modelPath = `${program.service}/models/${program.service}.model.js`;
+    if (program.database === 'mongodb') {
+        ENV_TEMPLATE += `
+MONGO_DATABASE_URI=mongodb://localhost:27017/${program.service}
+    `;
+    } else if (program.database === 'postgres') {
+        ENV_TEMPLATE += `
+POSTGRES_DATABASE_URI=postgres://postgres:password@localhost:5432/${program.service}
+    `;
+    } else {
+        console.log("Wrong database parameter, only support: mongodb and postgres");
+        process.exit(0);
+    }
+
+    fs.writeFileSync(
+        modelPath,
+        program.database === 'mongodb' ? MONGODB_MODEL_TEMPLATE : POSTGRES_MODEL_TEMPLATE,
+        {encoding: 'utf8'});
+}
+
+fs.writeFileSync(
+    `${program.service}/Dockerfile`,
+    DOCKER_TEMPLATE,
+    {encoding: 'utf8'});
+
+fs.writeFileSync(
+    `${program.service}/.env.local`,
+    ENV_TEMPLATE,
+    {encoding: 'utf8'});
+
+// Update package.json
+let pkg = fs.readFileSync(
+    `${program.service}/package.json`, {encoding: 'utf8'});
+
+fs.writeFileSync(
+    `${program.service}/package.json`,
+    pkg.replace("{service}", `${capitalize(program.service)}Service`),
+    {encoding: 'utf8'});
+
+// Update modules
+let  modules = fs.readFileSync(
+    `${program.service}/modules/index.js`, {encoding: 'utf8'});
+
+fs.writeFileSync(
+    `${program.service}/modules/index.js`,
+    `
+    ${program.auth ? `import ${program.auth} from './${program.auth}.js'` : ''}
+    ${modules.replace("correlation,", MIDDLEWARES)}
+    `,
+    {encoding: 'utf8'});
+
+console.log("Install modules")
+
+exec(`cd ${program.service}; npm install`, (err)=> {
+    if(err) return  console.log(err);
+    console.log("Finished");
+}).stdout
     .pipe(process.stdout);
---
+++ bin/templates.js
@@ -0,0 +1,118 @@
+import {exec} from "child_process";
+
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+function execAsync(command){
+    return new Promise((resolve, reject)=> {
+        exec(command, (err)=> {
+            if(err)
+                return reject(err);
+
+            resolve();
+        });
+    });
+}
+
+export {
+    capitalize,
+    execAsync
+}
+export default function (program) {
+
+    const PROTO_TEMPLATE = `
+syntax = "proto3";
+
+import "google/protobuf/duration.proto";
+import "google/protobuf/empty.proto";
+import "google/api/annotations.proto";
+import "validation.proto";
+
+service ${capitalize(program.service)}Service {
+  rpc test (google.protobuf.Empty) returns (Test) {
+      option (google.api.http) = {
+      get: "/${program.service}/me"
+    };
+  }
+}
+
+message Test {
+  string id = 1;
+}
+`;
+
+    const SERVICE_TEMPLATE = `
+export default class ${capitalize(program.service)}Service {
+    static proto = '${program.service}.proto';
+
+    static async test() {
+      return { id: 'test' };
+    }
+}
+`;
+
+    const TEST_TEMPLATE = `
+/* eslint-disable no-undef */
+import ${capitalize(program.service)}Service from '../../services/${program.service}.service.js';
+
+describe('${program.service}.service.js', () => {
+    it('If ${capitalize(program.service)}Service is initialized, static field "proto" exist in ${capitalize(program.service)}Service', () => {
+        expect(${capitalize(program.service)}Service).toHaveProperty("proto");
+    });
+});
+`;
+
+    const MONGODB_MODEL_TEMPLATE = `
+import mongoose from 'mongoose';
+
+const ${capitalize(program.service)}Schema = new mongoose.Schema(
+    {
+        name: String
+    }, {
+        timestamps: true,
+        collection: "${program.service}"
+    }
+);
+
+const Model = mongoose.model('${capitalize(program.service)}', ${capitalize(program.service)}Schema);
+export default Model;
+`;
+
+    const POSTGRES_MODEL_TEMPLATE = `
+const ${capitalize(program.service)} = (sequelize, { DataTypes }) => sequelize.define('${program.service}', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+});
+
+export default ${capitalize(program.service)};
+`;
+
+    let ENVIRONMENT_TEMPLATE = `
+PORT=${program.port}
+`;
+
+    const DOCKER_TEMPLATE = `
+FROM node:15-alpine as build
+RUN apk add bash
+WORKDIR /src/app
+COPY package*.json ./
+COPY . .
+RUN npm install
+EXPOSE ${program.port} ${Number(program.port) + 1}
+CMD ["npm","start"]
+`;
+
+    return {
+        PROTO_TEMPLATE,
+        SERVICE_TEMPLATE,
+        TEST_TEMPLATE,
+        MONGODB_MODEL_TEMPLATE,
+        POSTGRES_MODEL_TEMPLATE,
+        ENVIRONMENT_TEMPLATE,
+        DOCKER_TEMPLATE
+    }
+}
